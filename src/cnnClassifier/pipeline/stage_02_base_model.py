672f30d759b1b11c7e75225602e3c4f9aa9d4bf3
palakvan <vanpariyapalak4@gmail.com>
2024-04-05 10:40:28 +0530

    Data Ingestion Added

---
+++ src/cnnClassifier/pipeline/stage_02_base_model.py
@@ -0,0 +1,8 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+
+STAGE_NAME = "Prepare Base Model"
+
+
