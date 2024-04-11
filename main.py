31fea0a524708fbe38a0848fbf9b04a132f014dc
palakvan <vanpariyapalak4@gmail.com>
2024-04-11 15:56:13 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ os.environ["MLFLOW_TRACKING_PASSWORD"]="038196cf537e025d3694b350d62495e26ff48315
 
 
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#         data_ingestion = DataIngestionTrainingPipeline()
-#         data_ingestion.main()
-#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
     
-# STAGE_NAME = "Prepare Base Model"
-# try: 
-#         logger.info(f"*******************")
-#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#         obj = PrepareBaseModelTrainingPipeline()
-#         obj.main()
-#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare Base Model"
+try: 
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 STAGE_NAME = "Training"
 try:
