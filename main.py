54bee9b6e927b56d4d9ea113d684d4fcdcbb05bf
palakvan <vanpariyapalak4@gmail.com>
2024-04-11 13:24:59 +0530

    model evaluation

--- main.py
+++ main.py
@@ -2,29 +2,35 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+import os
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/palakvan/Kidney_Disease_Classification.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="palakvan"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="038196cf537e025d3694b350d62495e26ff48315"
 
 
 
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#         data_ingestion = DataIngestionTrainingPipeline()
+#         data_ingestion.main()
+#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
     
-STAGE_NAME = "Prepare Base Model"
-try:
-        logger.info(f"*******************")
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+# STAGE_NAME = "Prepare Base Model"
+# try: 
+#         logger.info(f"*******************")
+#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#         obj = PrepareBaseModelTrainingPipeline()
+#         obj.main()
+#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
 
 STAGE_NAME = "Training"
 try:
@@ -33,6 +39,19 @@ try:
         obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,8 @@ from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -72,3 +73,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney_disease_data",
+            mlflow_uri="https://dagshub.com/palakvan/Kidney_Disease_Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
